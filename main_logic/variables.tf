d88e2e08746dfea271b0f81c66759b48f6bcff8e
hvorostok <hvorostok8@gmail.com>
2025-10-03 07:12:44 +0300

    feat: add vpc with subnets

--- main_logic/variables.tf
+++ main_logic/variables.tf
@@ -20,3 +20,22 @@ variable "s3_folder_name" {
   description = "Prefix for tfstate in S3 bucket"
   type        = string
 }
+
+data "yandex_compute_image" "latest_ubuntu_2404" {
+  family = "ubuntu-2404-lts"
+}
+locals {
+  ubuntu_id = data.yandex_compute_image.latest_ubuntu_2404.id
+}
+
+variable "vpc_network_name" {
+  description = "VPC wth 3 subnets for inst, db & public traffic"
+  type        = string
+}
+variable "subnets" {
+  description = "A map of subnets to create"
+  type = map(object({
+    cidr_block = string
+    name       = string
+  }))
+}
