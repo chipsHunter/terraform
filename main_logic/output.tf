dc63d7812695688b226ab1d97c91720370de06ad
hvorostok <hvorostok8@gmail.com>
2025-10-04 20:29:00 +0300

    fix: cidr blocks

--- main_logic/output.tf
+++ main_logic/output.tf
@@ -5,3 +5,15 @@ data "yandex_compute_image" "latest_ubuntu" {
 output "ubuntu_image" {
   value = data.yandex_compute_image.latest_ubuntu.description
 }
+
+output "created_subnets_info" {
+  description = "Detailed information for each created subnet"
+  value = {
+    for key, subnet in yandex_vpc_subnet.all_subnets :
+    key => {
+      id         = subnet.id
+      name       = subnet.name
+      cidr_block = subnet.v4_cidr_blocks[0]
+    }
+  }
+}
